050b2fa5e676c68d5dc475e7ba87cea27bc67ac0
Roy Alameh <roy.alameh24@d155.org>
2022-11-29 13:10:54 -0600

    Added Buttons, added images, added image highlighting upon selection, fixed touch gesture

--- Rock Paper Scissors/GameViewController.swift
+++ Rock Paper Scissors/GameViewController.swift
@@ -8,13 +8,64 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet weak var RockOutlet: UIButton!
+    @IBOutlet weak var PaperOutlet: UIButton!
+    @IBOutlet weak var ScissorsOutlet: UIButton!
+    
+    @IBOutlet weak var RockView: UIImageView!
+    @IBOutlet weak var PaperView: UIImageView!
+    
+    @IBOutlet weak var ScissorsView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let radius = 15
+        RockOutlet.layer.cornerRadius = CGFloat(radius)
+        PaperOutlet.layer.cornerRadius = CGFloat(radius)
+        ScissorsOutlet.layer.cornerRadius = CGFloat(radius)
+        
+        RockView.layer.masksToBounds = true
+        RockView.layer.borderWidth = 5
+        RockView.layer.borderColor = UIColor.white.cgColor
+        PaperView.layer.masksToBounds = true
+        PaperView.layer.borderWidth = 5
+        PaperView.layer.borderColor = UIColor.white.cgColor
+        ScissorsView.layer.masksToBounds = true
+        ScissorsView.layer.borderWidth = 5
+        ScissorsView.layer.borderColor = UIColor.white.cgColor
         
     }
-
-    @IBAction func tapRecognition(_ sender: Any) {
+    
+    @IBAction func RockAction(_ sender: UIButton) {
+        RockView.layer.masksToBounds = true
+        RockView.layer.borderWidth = 5
+        RockView.layer.borderColor = UIColor.systemBlue.cgColor
         
+        PaperView.layer.borderColor = UIColor.white.cgColor
+        ScissorsView.layer.borderColor = UIColor.white.cgColor
+    }
+    @IBAction func PaperAction(_ sender: UIButton) {
+        PaperView.layer.masksToBounds = true
+        PaperView.layer.borderWidth = 5
+        PaperView.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        RockView.layer.borderColor = UIColor.white.cgColor
+        ScissorsView.layer.borderColor = UIColor.white.cgColor
+        
+    }
+    @IBAction func ScissorsAction(_ sender: UIButton) {
+        ScissorsView.layer.masksToBounds = true
+        ScissorsView.layer.borderWidth = 5
+        ScissorsView.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        RockView.layer.borderColor = UIColor.white.cgColor
+        PaperView.layer.borderColor = UIColor.white.cgColor
+        
+    }
+    
+    
+    
+    @IBAction func tapOutlet(_ sender: Any) {
+        print("Hello")
     }
 }
