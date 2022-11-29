883ba06c6df9f5e726782da01ce4b249b5ed6fa7
Roy Alameh <roy.alameh24@d155.org>
2022-11-29 09:00:31 -0600

    Added triple tap gesture recognizer

---
+++ Rock Paper Scissors/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by ROY ALAMEH on 11/29/22.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func tapRecognition(_ sender: Any) {
+        
+    }
+}
