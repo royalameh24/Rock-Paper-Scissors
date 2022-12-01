3b951b9bc0632e1437da6eb678fb8d4d735ed866
Roy Alameh <roy.alameh24@d155.org>
2022-12-01 08:10:50 -0600

    Added table cell

--- Rock Paper Scissors/GameViewController.swift
+++ Rock Paper Scissors/GameViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 import simd
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        winner.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = TableOutlet.dequeueReusableCell(withIdentifier: "resultCell")!
+        cell.textLabel?.text = winner[indexPath.row]
+        return cell
+    }
+    
+    var winner = ["Play to win!"]
+    var winningMove = ["Beat the computer!"]
+    var computerPerception = ["Use the slider to increase or decrease the comptuer perception"]
+    
+    
     
     @IBOutlet weak var RockOutlet: UIButton!
     @IBOutlet weak var PaperOutlet: UIButton!
@@ -18,6 +34,8 @@ class GameViewController: UIViewController {
     @IBOutlet weak var ScissorsView: UIImageView!
     @IBOutlet weak var InformationOutlet: UILabel!
     @IBOutlet weak var ComputerView: UIImageView!
+    @IBOutlet weak var SliderOutlet: UISlider!
+    @IBOutlet weak var TableOutlet: UITableView!
     
     let dictionaryNames = ["Rock" : 1, "Paper" : 2, "Scissors" : 3]
     let dictionaryNums = [1: "Rock", 2 : "Paper", 3: "Scissors"]
@@ -26,6 +44,9 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        TableOutlet.delegate = self
+        TableOutlet.dataSource = self
+        
         let radius = 15
         RockOutlet.layer.cornerRadius = CGFloat(radius)
         PaperOutlet.layer.cornerRadius = CGFloat(radius)
@@ -53,6 +74,8 @@ class GameViewController: UIViewController {
         
         PaperView.layer.borderColor = UIColor.white.cgColor
         ScissorsView.layer.borderColor = UIColor.white.cgColor
+        
+        selection = 1
     }
     @IBAction func PaperAction(_ sender: UIButton) {
         PaperView.layer.masksToBounds = true
@@ -61,6 +84,8 @@ class GameViewController: UIViewController {
         
         RockView.layer.borderColor = UIColor.white.cgColor
         ScissorsView.layer.borderColor = UIColor.white.cgColor
+        
+        selection = 2
         
     }
     @IBAction func ScissorsAction(_ sender: UIButton) {
@@ -71,14 +96,36 @@ class GameViewController: UIViewController {
         RockView.layer.borderColor = UIColor.white.cgColor
         PaperView.layer.borderColor = UIColor.white.cgColor
         
+        selection = 3
     }
     
     
     
     @IBAction func tapOutlet(_ sender: Any) {
-        guard selection == 0 else {
+        print("Hello")
+        guard selection != 0 else {
             InformationOutlet.text = "No selection made, select before entering (triple tap)"
+            clearSelection()
             return
         }
+        let rand = Int(Double.random(in: 0...1) * 1000000000)
+        let sliderNum = Int(SliderOutlet.value * 1000000000)
+        if (rand <= sliderNum) {
+            let computerChoice = lossDictionary[dictionaryNums[selection]!]!
+            InformationOutlet.text = computerChoice
+        }
+        else {
+            let newRand = Int.random(in: 1...3)
+            InformationOutlet.text = dictionaryNums[newRand]!
+        }
+        
+        clearSelection()
+    }
+    
+    func clearSelection() {
+        RockView.layer.borderColor = UIColor.white.cgColor
+        PaperView.layer.borderColor = UIColor.white.cgColor
+        ScissorsView.layer.borderColor = UIColor.white.cgColor
+        selection = 0
     }
 }
